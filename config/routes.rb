6efe042106980e18d3c123769be2720373e3a478
Julio Navarro <jnavarro@able.co>
2020-05-29 15:47:34 -0500

    Add navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'memes#index'
   get "/popular", to: "memes#popular"
-  resources :memes, only: [:show]
+  resources :memes, only: [:show, :new, :create]
   resources :categories, only: [:index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
